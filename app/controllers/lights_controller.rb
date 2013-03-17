2cf3570d256ad3451775b28a9e522528622e3017
Vishal Sakaria <vsakaria@gmail.com>
2013-03-17 21:55:17 +0000

    Create light then display all light for a user

--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -1,35 +1,34 @@
+
+
 class LightsController < ApplicationController
   # GET /lights
   # GET /lights.json
   def index
+
     @lights = Light.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @lights }
-    end
+
   end
 
   # GET /lights/1
   # GET /lights/1.json
   def show
-    @light = Light.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @light }
-    end
+    @user = User.find(params[:user_id])
+    @lights = Light.where(:user_id => params[:user_id])
+
+
+   
   end
 
   # GET /lights/new
   # GET /lights/new.json
   def new
+    
+    @user = User.find(params[:user_id])
+
     @light = Light.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @light }
-    end
   end
 
   # GET /lights/1/edit
@@ -37,47 +36,31 @@ class LightsController < ApplicationController
     @light = Light.find(params[:id])
   end
 
-  # POST /lights
-  # POST /lights.json
   def create
-    @light = Light.new(params[:light])
 
-    respond_to do |format|
-      if @light.save
-        format.html { redirect_to @light, notice: 'Light was successfully created.' }
-        format.json { render json: @light, status: :created, location: @light }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @light.errors, status: :unprocessable_entity }
-      end
-    end
+    #How could I refactor this?
+    @user = User.find(params[:user_id])
+    
+    @light = @user.lights.build(params[:light])
+    @light.save!
+
+
+    redirect_to(user_light_path(@user, @light))
+
   end
 
-  # PUT /lights/1
-  # PUT /lights/1.json
+
   def update
+
     @light = Light.find(params[:id])
 
-    respond_to do |format|
-      if @light.update_attributes(params[:light])
-        format.html { redirect_to @light, notice: 'Light was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @light.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /lights/1
-  # DELETE /lights/1.json
+
   def destroy
+
     @light = Light.find(params[:id])
     @light.destroy
 
-    respond_to do |format|
-      format.html { redirect_to lights_url }
-      format.json { head :no_content }
-    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
+    
     @users = User.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
